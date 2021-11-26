a9d25b4e48df7be6d120e16767186dc2e28f2802
Patrick Crowne <Patrick.crowne@gmail.com>
2021-11-25 17:57:21 -0800

    Initial Commit

---
+++ plang/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  plang
+//
+//  Created by Patrick Crowne on 11/25/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
